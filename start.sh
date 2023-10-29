eca8f23c3580927970f81a00d3e5f9f1a4be1d48
D S I GUNASEKARA <148281527+shamitum@users.noreply.github.com>
2023-10-29 22:46:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,49 +11,103 @@ rm ./Xray-linux-64.zip
 cat > ./config.json <<EOF
 {
     "log": {
-        "loglevel": "warning"
-    },
-    "routing": {
-        "domainStrategy": "AsIs",
-        "rules": [
-            {
-                "type": "field",
-                "ip": [
-                    "geoip:private"
-                ],
-                "outboundTag": "block"
-            }
-        ]
+        "loglevel": "info"
     },
     "inbounds": [
         {
+            "tag": "VMess TCP",
             "listen": "0.0.0.0",
-            "port": 80,
+            "port": 8081,
             "protocol": "vmess",
             "settings": {
-                "clients": [
-                    {
-                        "id": "40b1623c-6cfe-44ef-b6f6-3e9aefc1f0c7"
+                "clients": []
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "tcpSettings": {
+                    "header": {
+                        "type": "http",
+                        "request": {
+                            "method": "GET",
+                            "path": [
+                                "/"
+                            ],
+                            "headers": {
+                                "Host": [
+                                    "google.com"
+                                ]
+                            }
+                        },
+                        "response": {}
                     }
+                },
+                "security": "none"
+            },
+            "sniffing": {
+                "enabled": true,
+                "destOverride": [
+                    "http",
+                    "tls"
                 ]
+            }
+        },
+        {
+            "tag": "VMess Websocket",
+            "listen": "0.0.0.0",
+            "port": 8080,
+            "protocol": "vmess",
+            "settings": {
+                "clients": []
             },
             "streamSettings": {
                 "network": "ws",
+                "wsSettings": {
+                    "path": "/",
+                    "headers": {
+                        "Host": "google.com"
+                    }
+                },
                 "security": "none"
+            },
+            "sniffing": {
+                "enabled": true,
+                "destOverride": [
+                    "http",
+                    "tls"
+                ]
             }
         }
+        
     ],
     "outbounds": [
         {
             "protocol": "freedom",
-            "tag": "direct"
+            "tag": "DIRECT"
         },
         {
             "protocol": "blackhole",
-            "tag": "block"
+            "tag": "BLOCK"
         }
-    ]
+    ],
+    "routing": {
+        "rules": [
+            {
+                "ip": [
+                    "geoip:private"
+                ],
+                "domain": [
+                    "geosite:private"
+                ],
+                "protocol": [
+                    "bittorrent"
+                ],
+                "outboundTag": "BLOCK",
+                "type": "field"
+            }
+        ]
+    }
 }
+
 EOF
 
 ./xray -c ./config.json
